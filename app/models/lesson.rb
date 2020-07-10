d259b4584d1aec85b2d4e3650a77c292d4cfb193
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2020-07-10 09:53:35 -0700

    Return relations' ids in JSON of Lesson and Task

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,11 @@
 class Lesson < ApplicationRecord
   belongs_to :category
   has_and_belongs_to_many :tasks
+
+  def as_json(options = {})
+    super(options).merge({
+      'category_id' => self.category.id,
+      'tasks_ids' => self.tasks.map(&:id)
+    })
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,11 @@
 class Task < ApplicationRecord
   has_and_belongs_to_many :lessons
   has_many :categories, through: :lessons
+
+  def as_json(options = {})
+    super(options).merge({
+      'categories_ids' => self.categories.map(&:id),
+      'lessons_ids' => self.lessons.map(&:id)
+    })
+  end
 end
